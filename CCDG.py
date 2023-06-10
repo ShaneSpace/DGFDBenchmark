7563eb60aa3aabeb0ae0304e955f1e8c1e58e7aa
ShaneSpace <jialinshan123@gmail.com>
2023-06-10 14:48:44 +0800

    update the code of ccdg

--- CCDG.py
+++ CCDG.py
@@ -9,6 +9,10 @@ from torch.autograd import Variable
 # from torchvision import models
 from torchsummary import summary
 
+from models.Conv1dBlock import Conv1dBlock
+from models.Networks import Network_bearing, Network_fan
+from datasets.load_bearing_data import ReadCWRU, ReadDZLRSB, ReadJNU, ReadPU, ReadMFPT, ReadUOTTAWA
+from datasets.load_fan_data import ReadMIMII
 
 import numpy as np
 import yaml
@@ -28,11 +32,6 @@ import matplotlib.pyplot as plt
 import math
 
 
-from models.Conv1dBlock import Conv1dBlock
-from models.Networks import Network_bearing, Network_fan
-from datasets.load_bearing_data import ReadCWRU, ReadDZLRSB, ReadJNU, ReadPU, ReadMFPT, ReadUOTTAWA
-from datasets.load_fan_data import ReadMIMII
-
 # self-made utils
 from utils.DictObj import DictObj
 from utils.AverageMeter import AverageMeter
@@ -44,56 +43,86 @@ from utils.TuneReport import GenReport
 from utils.DatasetClass import InfiniteDataLoader, SimpleDataset
 # from utils.SignalTransforms import AddGaussianNoise, RandomScale, MakeNoise, Translation
 # from utils.LMMD import LMMDLoss
-# from utils.GradientReserve import grad_reverse
+from utils.GradientReserve import grad_reverse
 
 # run code
-# srun -w node3 --gres=gpu:1  /home/lsjia4/anaconda3/envs/pytorch/bin/python /home/lsjia4/MyFolder/fault_diagnosis/DGFDBenchmark/WhiteningNet.py
+# srun -w node3 --gres=gpu:1  /home/lsjia4/anaconda3/envs/pytorch/bin/python /home/lsjia4/MyFolder/fault_diagnosis/DGFDBenchmark/CCDG.py
 
 
-
-
-
-with open(os.path.join(sys.path[0], 'config_files/WhiteningNet_config.yaml')) as f:
-    '''
-    link: https://zetcode.com/python/yaml/#:~:text=YAML%20natively%20supports%20three%20basic,for%20YAML%3A%20PyYAML%20and%20ruamel.
-    '''
+with open(os.path.join(sys.path[0], 'config_files/CCDG_config.yaml')) as f:
     configs = yaml.load(f, Loader=yaml.FullLoader)
-    print(configs) #yaml库读进来的是字典dict格式，需要用DictObj将dict转换成object
+    print(configs)
     configs = DictObj(configs)
 
     if configs.use_cuda and torch.cuda.is_available():
         # set(configs,'device','cuda')
         configs.device='cuda'
 
-#%% model
 
-
-class CausalLoss(nn.Module):
-
+class DomainContrastiveLoss(nn.Module):
+    '''
+    Copied from
+    https://github.com/mohamedr002/CCDG
+    '''
     def __init__(self, configs):
         super().__init__()
-        self.num_classes = configs.num_classes
+        self.configs = configs
+        self.device = configs.device
 
-    def forward(self, flatten_features, data_label):
-        list_category = [[] for i in range(self.num_classes)]
-        # list_label = [[] for i in range(self.num_classes)] # for debug
-        for i, fv in zip(data_label, flatten_features):
-            fv = torch.reshape(fv, (1, fv.size(0)))
-            list_category[i].append(fv)
-            # list_label[i].append(i) # for debug
-        # self.list_label = list_label# for debug
+    def forward(self, domains_features, domains_labels, temperature=0.7):
+            # masking for the corresponding class labels.
+        anchor_feature = domains_features
+        anchor_feature = F.normalize(anchor_feature, dim=1)
+        labels = domains_labels
+        labels= labels.contiguous().view(-1, 1)
+        # Generate masking for positive and negative pairs.
+        mask = torch.eq(labels, labels.T).float().to(self.device)
+        # Applying contrastive via product among the features
+        # Measure the similarity between all samples in the batch
+        # reiterate fact from Linear Algebra if u and v two vectors are normalised implies cosine similarity
+        anchor_dot_contrast = torch.div(torch.matmul(anchor_feature, anchor_feature.T), temperature)
 
-        total_causal_loss = 0
-        for i in range(self.num_classes):
-            fm_i = torch.cat(tuple(list_category[i]), dim=0) # convert the feature vector listinto a single tensor(matrix)
-            # print(fm_i.shape)
-            fm_i_mean = torch.mean(fm_i, dim=0, keepdim=True)
-            causal_i = torch.sum(torch.mean((fm_i - fm_i_mean).pow(2), dim=0))
-            total_causal_loss = total_causal_loss + causal_i
+        # for numerical stability
+        # substract max value from the output
+        logits_max, _ = torch.max(anchor_dot_contrast, dim=1, keepdim=True)
+        logits = anchor_dot_contrast - logits_max.detach()
 
-        return total_causal_loss
+        # create inverted identity matrix with same shape as mask.
+        # only diagnoal is zeros and all others are ones
+        logits_mask = torch.scatter(torch.ones_like(mask), 1, torch.arange(anchor_feature.shape[0]).view(-1, 1).to(self.device), 0)
 
-class WhitenNet(nn.Module):
+        # 上边这个完全可以使用： logits_mask = 1-torch.eye(mask.shape[0])
+
+        # mask-out self-contrast cases
+        # the diagnoal represent same samples similarity i=j>> we need to remove this
+        # remove the true labels mask
+        # all ones in mask would be only samples with same labels
+        mask = mask * logits_mask
+
+        # compute log_prob and remove the diagnal
+        # remove same features from the normalized contrastive matrix
+        # The denominoator of the equation samples
+        exp_logits = torch.exp(logits) * logits_mask
+
+        # substract the whole multiplications from the negative pair and positive pairs
+        log_prob = logits - torch.log(exp_logits.sum(1, keepdim=True))
+
+        # compute mean of log-likelihood over positive
+        mask_sum = mask.sum(1)
+        zeros_idx = torch.where(mask_sum == 0)[0]
+        mask_sum[zeros_idx] = 1
+
+        mean_log_prob_pos = (mask * log_prob).sum(1) / mask_sum
+
+        # loss
+        loss = (- 1 * mean_log_prob_pos)
+        loss = loss.mean()
+
+        return loss
+
+
+
+class CCDG(nn.Module):
     def __init__(self, configs):
         super().__init__()
         self.configs = configs
@@ -105,78 +134,47 @@ class WhitenNet(nn.Module):
         self.lr = configs.lr
         self.batch_size = configs.batch_size
         self.use_domain_weight = configs.use_domain_weight
-        if self.dataset_type == 'bearing':
+
+        self.domain_contrastive_loss = DomainContrastiveLoss(configs).to(self.device)
+        self.cross_entropy_loss = nn.CrossEntropyLoss(reduction='none')
+
+        if self.dataset_type=='bearing':
             self.model = Network_bearing(configs).to(self.device)
-        elif self.dataset_type == 'fan':
+        elif self.dataset_type=='fan':
             self.model = Network_fan(configs).to(self.device)
-
-        self.optimizer = torch.optim.Adam(list(self.model.parameters()), lr=self.lr)
-
-        self.cl_loss = nn.CrossEntropyLoss(reduction='none')
-        self.causal_loss = CausalLoss(configs)
-
-        self.lamda_causal = configs.lamda_causal
-        self.num_domains = len(configs.datasets_src)
+        else:
+            raise ValueError('The dataset_type should be bearing or fan!')
+        self.optimizer = torch.optim.SGD(list(self.model.parameters()), lr=self.lr)
         self.weight_step = None
-        self.use_learning_rate_sheduler = configs.use_learning_rate_sheduler
-
-
-
-    def adjust_learning_rate(self, step):
-        """
-        Decay the learning rate based on schedule
-        https://github.com/facebookresearch/moco/blob/main/main_moco.py
-        """
-        lr = self.lr
-        if self.configs.cos:  # cosine lr schedule
-            lr *= 0.5 * (1.0 + math.cos(math.pi * step / self.steps))
-        else:  # stepwise lr schedule
-            for milestone in self.configs.schedule:
-                lr *= 0.5 if step >= milestone else 1.0
-        for param_group in self.optimizer.param_groups:
-            param_group["lr"] = lr
-
 
     def update(self, minibatches):
         x = torch.cat([x for x, y in minibatches]) # the length of the inner list is the number of the source domains (one machine is corresponding to a domain)
-        # debug
-        # x_shape = [x.shape[0] for x,y in minibatches]
-        # print('The shape of X',x_shape)
         labels = torch.cat([y for x, y in minibatches])
         x      = x.to(self.device)
         labels = labels.to(self.device)
 
-        feature_vectors, logits = self.model(x)
+        fv, logits = self.model(x)
 
-        if self.use_domain_weight:
-            if  self.weight_step is None:
-                self.weight_step = torch.ones(x.shape[0]).to(self.device)
-            else:
-                ce_values  = self.cl_loss(logits, labels)
-                ce_values_2d = torch.reshape(ce_values, (self.num_domains, self.batch_size))
-                ce_value_domain = torch.mean(ce_values_2d,dim=1)
-                ce_value_sum = torch.sum(ce_value_domain)
-                weight_step = 1 + ce_value_domain/ce_value_sum
-                self.weight_step = weight_step.repeat((self.batch_size,1)).T.flatten(0).to(self.device)
-        else:
+        if  self.weight_step is None:
             self.weight_step = torch.ones(x.shape[0]).to(self.device)
 
-        cl_loss = torch.mean(self.cl_loss(logits, labels)*self.weight_step)
-        causal_loss = self.causal_loss(feature_vectors, labels)
-        total_loss = cl_loss + self.lamda_causal*causal_loss
+        # ce_loss = F.cross_entropy(logits, labels)
+        ce_loss = torch.mean(self.cross_entropy_loss(logits, labels)*self.weight_step)
 
+        assert not torch.any(torch.isnan(fv)) #这个loss是施加在logits上的
+        dc_loss = self.domain_contrastive_loss(fv, labels)
+
+        loss = ce_loss + dc_loss
         self.optimizer.zero_grad()
-        total_loss.backward()
+        loss.backward()
+        # with torch.autograd.detect_anomaly():
+        #     loss.backward()
         self.optimizer.step()
 
-        loss_cl = cl_loss.detach().cpu().data.numpy()
-        loss_causal = causal_loss.detach().cpu().data.numpy()
-
-
-        losses={}
-
-        losses['cl'] = loss_cl
-        losses['causal'] = loss_causal
+        losses = {}
+        losses['loss_total'] = loss.detach().cpu().data.numpy()
+        losses['loss_ce'] = ce_loss.detach().cpu().data.numpy()
+        losses['loss_id'] = dc_loss.detach().cpu().data.numpy()
 
         return losses
 
@@ -184,8 +182,7 @@ class WhitenNet(nn.Module):
         self.logger = logger
         self.to(self.device)
 
-        # loss_acc_result = {'loss_cc': [], 'loss_ct':[], 'loss_cl':[], 'acces':[]}
-        loss_acc_result = {'loss_causal': [], 'loss_cl':[], 'acces':[]}
+        loss_acc_result = {'loss_total': [], 'loss_ce':[], 'loss_id':[], 'acces':[]}
 
         for step in range(1, self.steps+1):
             self.train()
@@ -193,30 +190,26 @@ class WhitenNet(nn.Module):
             self.logger.info("================Step {}================".format(step))
             minibatches_device = next(train_minibatches_iterator)
             losses = self.update(minibatches_device)
-            # self.scheduler.step()
-            if self.use_learning_rate_sheduler:
-                self.adjust_learning_rate(self.current_step)
 
-            # loss_acc_result['loss_cc'].append(losses['cc'])
-            # loss_acc_result['loss_ct'].append(losses['ct'])
-            loss_acc_result['loss_cl'].append(losses['cl'])
-            loss_acc_result['loss_causal'].append(losses['causal'])
+            loss_acc_result['loss_total'].append(losses['loss_total'])
+            loss_acc_result['loss_ce'].append(losses['loss_ce'])
+            loss_acc_result['loss_id'].append(losses['loss_id'])
+            # print(losses['loss_total'])
+            # print(losses['loss_ce1'])
+            # print(losses['loss_ce2'])
 
-            # self.logger.info('loss_cl_train: \t {loss_cl: .4f} \t loss_cc_train: \t {loss_cc: .4f} \t loss_ct_train: \t {loss_ct: .4f} \t loss_cl_train: \t {loss_cl: .4f}'.format(loss_cl=losses['cl'], loss_cc=losses['cc'], loss_ct=losses['ct']))
-            self.logger.info('loss_cl_train: \t {loss_cl: .4f} \t loss_proto_train: \t {loss_causal: .4f} '.format(loss_cl=losses['cl'], loss_causal=losses['causal']))
-
+            self.logger.info('loss_total_train: \t {loss_total: .4f} \t loss_ce_train: \t {loss_ce: .4f} \t loss_id_train: \t {loss_id: .4f}'.format(loss_total=losses['loss_total'], loss_ce=losses['loss_ce'], loss_id=losses['loss_id']))
 
             #显示train_accuracy和test_accuracy
             if step % self.checkpoint_freq == 0 or step==self.steps:
                 acc_results = self.test_model(test_loaders)
                 loss_acc_result['acces'].append(acc_results)
-                # 2023/03/19
-                # this part can be used to calculate the weight of domains
-                # if self.use_domain_weight:
-                #     weight_step = torch.from_numpy(1-np.array(acc_results[1:])).type(torch.float32)
-                #     self.weight_step = weight_step.repeat((self.batch_size,1)).T.flatten(0).to(self.device)
-                # else:
-                #     self.weight_step = None
+                #################
+                if self.use_domain_weight:
+                    weight_step = torch.from_numpy(1-np.array(acc_results[1:])).type(torch.float32)
+                    self.weight_step = weight_step.repeat((self.batch_size,1)).T.flatten(0).to(self.device)
+                else:
+                    self.weight_step = None
 
                 self.logger.info('tgt_train_acc: \t {src_train_acc: .4f}'.format(src_train_acc=acc_results[0]))
                 for i in range(1,len(acc_results)):
@@ -236,9 +229,6 @@ class WhitenNet(nn.Module):
                 x, label_fault = batched_data
                 x = x.to(self.device)
                 label_fault = label_fault.to(self.device)
-                # # debug
-                # print(j)
-                # print(x.shape)
 
                 label_pred = self.predict(x)
                 y_pred_lst.extend(label_pred.detach().cpu().data.numpy())
@@ -250,12 +240,10 @@ class WhitenNet(nn.Module):
 
         return acc_results
 
-    def predict(self,x):
-        # print(x.shape)
-        _, logits = self.model(x)
-
-        return torch.max(logits, dim=1)[1]
-
+    def predict(self, x):
+        with torch.no_grad():
+            _, logits = self.model(x)
+            return torch.max(logits, dim=1)[1]
 
 def main(idx, configs):
     if configs.dataset_type == 'bearing':
@@ -286,7 +274,6 @@ def main(idx, configs):
     configs.datasets_tgt = datasets_tgt
     configs.datasets_src = datasets_src
 
-
     if configs.dataset_type == 'bearing':
         datasets_object_src = [eval('Read'+i+'(configs)') for i in datasets_src]
     elif configs.dataset_type=='fan':
@@ -305,42 +292,39 @@ def main(idx, configs):
 
     train_minibatches_iterator = zip(*train_loaders_src)
 
-    full_path_log = os.path.join('Output//WhiteningNet//log_files', datasets_list[idx])
+    full_path_log = os.path.join('Output//CCDG//log_files', datasets_list[idx])
     if not os.path.exists(full_path_log):
         os.makedirs(full_path_log)
 
-    full_path_rep = os.path.join('Output//WhiteningNet//TuneReport', datasets_list[idx])
+    full_path_rep = os.path.join('Output//CCDG//TuneReport', datasets_list[idx])
     if not os.path.exists(full_path_rep):
         os.makedirs(full_path_rep)
-
-
 
 
     currtime = str(time.time())[:10]
     logger = create_logger(full_path_log +'//log_file'+currtime)
     for i in range(1):
-        model = WhitenNet(configs)
-
+        model = CCDG(configs)
         for k, v in sorted(vars(configs).items()):
             logger.info('\t{}: {}'.format(k, v))
 
         loss_acc_result = model.train_model(train_minibatches_iterator, test_loaders_tgt+test_loaders_src, logger)
 
 
-        loss_acc_result['loss_cl'] = np.array(loss_acc_result['loss_cl'])
-        loss_acc_result['loss_causal'] = np.array(loss_acc_result['loss_causal'])
+
+        loss_acc_result['loss_total'] = np.array(loss_acc_result['loss_total'])
+        loss_acc_result['loss_ce'] = np.array(loss_acc_result['loss_ce'])
+        loss_acc_result['loss_id'] = np.array(loss_acc_result['loss_id'])
         loss_acc_result['acces']   = np.array(loss_acc_result['acces'])
 
 
         # # save the loss curve and acc curve
-        # sio.savemat('WhitenNet_reproduced//log_files//loss_acc_result'+currtime+'.mat',loss_acc_result)
-        # gen_report = GenReport('WhitenNet_reproduced//TuneReport//')
+
         sio.savemat(full_path_log+'//loss_acc_result'+currtime+'.mat',loss_acc_result)
         gen_report = GenReport(full_path_rep)
         gen_report.write_file(configs=configs, test_item=None, loss_acc_result=loss_acc_result)
         gen_report.save_file(currtime)
 
-        # torch.save(model.to('cpu'),'IDEA_test2//saved_models//model'+currtime+'.pt')
         currtime = str(time.time())[:10]
 
 
@@ -350,3 +334,14 @@ if __name__ == '__main__':
     # for i in range(5):
     #     main(i, configs)
 
+
+
+# mfs_data = read_mfs.read_data_file()
+# mfs_dataset_train = SimpleDataset(mfs_data['train'])
+# batch_data = mfs_dataset_train[:5]
+# x_out = the_model.forward(*batch_data)
+
+
+
+
+# %%
